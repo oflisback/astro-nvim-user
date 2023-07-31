d348790f641efa617ffa66de97fdeeac73bc2b0a
Ola Flisbäck <ola.flisback@gmail.com>
2023-07-31 18:09:00 +0200

    Disable alpha welcome screen

--- plugins/core.lua
+++ plugins/core.lua
@@ -22,6 +22,7 @@ return {
   },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
   --
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   -- {
