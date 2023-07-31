17c001403559e7ffef6fd37ddd2fcef507a69298
Ola Flisbäck <ola.flisback@gmail.com>
2023-07-31 19:04:12 +0200

    Delay train but not working for up down

---
+++ plugins/delaytrain.lua
@@ -0,0 +1,11 @@
+return {
+	"ja-ford/delaytrain.nvim",
+	config = function()
+		require("delaytrain").setup({
+			keys = {
+				["nv"] = { "h", "j", "k", "l", "w", "b" },
+				["nvi"] = { "<Left>", "<Down>", "<Up>", "<Right>" },
+			}})
+		end,
+	lazy = false
+	}
