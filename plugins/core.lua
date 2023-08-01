a1b400afaf1521d481e81411ba92fde4fb46b758
Ola Flisbäck <ola.flisback@gmail.com>
2023-08-01 19:08:46 +0200

    Disable nvim-notify

--- plugins/core.lua
+++ plugins/core.lua
@@ -23,6 +23,7 @@ return {
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
   --
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   -- {
