29fe36b4b6a358e6d1ec0726f6677534eaa69016
Ola Flisbäck <ola.flisback@gmail.com>
2023-08-02 13:34:34 +0200

    Format on save stuff, fixes markdown FoS

---
+++ README.md
@@ -0,0 +1,13 @@
+# Incomplete list of things to do after install
+
+## Install language servers
+
+Install with LspInstall, at least the following:
+
+- eslint
+- lua_ls
+- tsserver
+
+## Install null-ls
+
+Install source servers such as prettierd I guess.
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,30 +1,13 @@
+local with_root_file = function(...)
+  local files = { ... }
+  return function(utils) return utils.root_has_file(files) end
+end
+
 return {
   {
     "jay-babu/mason-null-ls.nvim",
     opts = {
       handlers = {
-        -- for prettier
-        prettier = function()
-          require("null-ls").register(require("null-ls").builtins.formatting.prettier.with {
-            condition = function(utils)
-              return utils.root_has_file "package.json"
-                or utils.root_has_file ".prettierrc"
-                or utils.root_has_file ".prettierrc.json"
-                or utils.root_has_file ".prettierrc.js"
-            end,
-          })
-        end,
-        -- for prettierd
-        prettierd = function()
-          require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
-            condition = function(utils)
-              return utils.root_has_file "package.json"
-                or utils.root_has_file ".prettierrc"
-                or utils.root_has_file ".prettierrc.json"
-                or utils.root_has_file ".prettierrc.js"
-            end,
-          })
-        end,
         -- For eslint_d:
         eslint_d = function()
           require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
@@ -35,6 +18,27 @@ return {
             end,
           })
         end,
+      },
+      onattach = function(client)
+        if client.server_capabilities.documentFormattingProvider then
+          vim.api.nvim_create_augroup("format_on_save", { clear = true })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            desc = "Auto format before save",
+            group = "format_on_save",
+            pattern = "<buffer>",
+            callback = function() vim.lsp.buf.formatting_sync { async = true } end,
+          })
+        end
+      end,
+      sources = {
+        require("null-ls").builtins.diagnostics.selene.with {
+          condition = with_root_file "selene.toml",
+        },
+        require("null-ls").builtins.completion.spell,
+        require("null-ls").builtins.diagnostics.eslint_d,
+        require("null-ls").builtins.formatting.black,
+        require("null-ls").builtins.formatting.prettierd,
+        require("null-ls").builtins.formatting.stylua,
       },
     },
   },
