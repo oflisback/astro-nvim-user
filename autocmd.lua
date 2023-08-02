28e0ff3fec7545cd8ae54a80ab6abc4c621db1a4
Ola Flisbäck <ola.flisback@gmail.com>
2023-08-02 13:29:56 +0200

    Some useful auto-commands

---
+++ autocmd.lua
@@ -0,0 +1,32 @@
+vim.cmd([[
+  augroup _general_settings
+    autocmd!
+      autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
+  augroup end
+
+  augroup _git
+    autocmd!
+    autocmd FileType gitcommit setlocal spell
+  augroup end
+
+  augroup _markdown
+    autocmd!
+    autocmd FileType markdown setlocal spell
+  augroup end
+
+  augroup _fugitive
+    autocmd!
+    autocmd BufReadPost fugitive://* set bufhidden=delete
+  augroup end
+
+]])
+
+-- Add timestamp as extension for backup files
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = vim.api.nvim_create_augroup("timestamp_backupext", { clear = true }),
+	desc = "Add timestamp to backup extension",
+	pattern = "*",
+	callback = function()
+		vim.opt.backupext = "-" .. vim.fn.strftime("%Y%m%d%H%M")
+	end,
+})
