9cdb045755487bfdc9163d9668f5acfb81a0623b
Ola Flisbäck <ola.flisback@gmail.com>
2023-08-19 18:37:44 +0200

    Fix eslint whine if missing eslint config

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -12,9 +12,7 @@ return {
         eslint_d = function()
           require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
             condition = function(utils)
-              return utils.root_has_file "package.json"
-                or utils.root_has_file ".eslintrc.json"
-                or utils.root_has_file ".eslintrc.js"
+              return utils.root_has_file ".eslintrc.json" or utils.root_has_file ".eslintrc.js"
             end,
           })
         end,
