82d762350af6cadc1c1a83f93037166a6dcd5fc8
Ola Flisbäck <ola.flisback@gmail.com>
2023-10-04 19:35:28 +0200

    Add vim-matchup

---
+++ plugins/vim-matchup.lua
@@ -0,0 +1,14 @@
+return {
+  "andymass/vim-matchup",
+  lazy = false,
+  require = "nvim-treesitter/nvim-treesitter",
+  after = "nvim-treesitter",
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      matchup = { enable = true },
+    }
+    vim.g.matchup_matchparen_offscreen = {}
+    vim.g.matchup_matchparen_deferred = 1
+    vim.api.nvim_del_keymap("", "z%")
+  end,
+}
