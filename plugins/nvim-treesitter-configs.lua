0fb03fdbefce03cefabadaf8c8b704d35b505ee0
Ola Flisbäck <ola.flisback@gmail.com>
2023-10-11 21:57:26 +0200

    Add nvim-treesitter-textobjects

--- plugins/nvim-treesitter-configs.lua
+++ plugins/nvim-treesitter-configs.lua
@@ -1,6 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  lazy = false,
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  lazy = true,
   config = function()
     require("nvim-treesitter.configs").setup {
       ensure_installed = {
@@ -21,6 +25,15 @@ return {
       indent = { enable = true },
       -- For vim-matchup
       matchup = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<A-space>",
+          node_incremental = "<A-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
     }
   end,
 }
