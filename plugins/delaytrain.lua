82ce27343c7e7653ff30a2e939336dc196a51219
Ola Flisbäck <ola.flisback@gmail.com>
2023-12-21 19:06:43 +0100

    Disable delaytrain for org files

--- plugins/delaytrain.lua
+++ plugins/delaytrain.lua
@@ -1,11 +1,13 @@
 return {
-	"ja-ford/delaytrain.nvim",
-	config = function()
-		require("delaytrain").setup({
-			keys = {
-				["nv"] = { "h", "j", "k", "l", "w", "b" },
-				["nvi"] = { "<Left>", "<Down>", "<Up>", "<Right>" },
-			}})
-		end,
-	lazy = false
-	}
+  "ja-ford/delaytrain.nvim",
+  config = function()
+    require("delaytrain").setup {
+      keys = {
+        ["nv"] = { "h", "j", "k", "l", "w", "b" },
+        ["nvi"] = { "<Left>", "<Down>", "<Up>", "<Right>" },
+      },
+      ignore_filetypes = { "org" },
+    }
+  end,
+  lazy = false,
+}
