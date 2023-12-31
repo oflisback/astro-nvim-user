2f53bd8db9e61178dcf6b7e831198d7fb438c1da
Ola Flisbäck <ola.flisback@gmail.com>
2023-12-31 14:22:32 +0100

    Org add todo command improvements

--- user_utils.lua
+++ user_utils.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.is_todo(line) return string.find(line, "TODO") or string.find(line, "DONE") end
 
 function M.get_indent_level(line)
+  if line == nil then return { 1, false } end
   local match = line:match "^(%*+)"
   local count = match and match:len() or 0
   local is_todo = false
@@ -16,21 +17,19 @@ function M.calc_todo_indent_level(lines_above_plus_current)
   local stars_on_line, is_todo = unpack(M.get_indent_level(current_line))
   if stars_on_line > 0 then
     -- Current line already has stars, respect that indentation level.
-    print "current line has stars, respecting it"
     return stars_on_line
   end
+
   -- Check lines above going upwards
   for index = #lines_above_plus_current - 1, 1, -1 do
     local line = lines_above_plus_current[index]
     stars_on_line, is_todo = unpack(M.get_indent_level(line))
     if stars_on_line > 0 then
       if is_todo then
-        print "inspected above line is todo, returning its number of stars"
         return stars_on_line
       else
         -- We found a header above the current line, the todo level should
         -- be that level plus one
-        print "inspected above line is header, returning its number of stars plus one"
         return stars_on_line + 1
       end
     end
@@ -45,6 +44,7 @@ function M.prepend_stars_to_current_line(nbr_stars)
   vim.api.nvim_set_current_line(new_line)
 end
 
+-- The main entry point to bind something to
 function M.apply_todo_transformation()
   local line = vim.api.nvim_get_current_line()
   local indent_level, _ = unpack(M.get_indent_level(line))
@@ -54,8 +54,8 @@ function M.apply_todo_transformation()
     local cursor = vim.api.nvim_win_get_cursor(0)
     local current_line_nbr = cursor[1] - 1
     local bufnr = vim.api.nvim_get_current_buf()
-    local top_line = math.max(1, current_line_nbr - 1)
-    local lines = vim.api.nvim_buf_get_lines(bufnr, top_line, current_line_nbr + 1, false)
+    local top_line_nbr = math.max(0, current_line_nbr - 1)
+    local lines = vim.api.nvim_buf_get_lines(bufnr, top_line_nbr, current_line_nbr + 1, false)
     M.prepend_stars_to_current_line(M.calc_todo_indent_level(lines))
     vim.cmd "startinsert"
     vim.cmd "normal! $"
