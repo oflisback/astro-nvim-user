2028004575f8097d309db2daed859b6fc2806335
Ola Flisbäck <ola.flisback@gmail.com>
2024-01-06 16:39:33 +0100

    Api key config for chatgpt

--- plugins/chatgpt.lua
+++ plugins/chatgpt.lua
@@ -3,6 +3,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("chatgpt").setup {
+      api_key_cmd = os.getenv "HOME" .. "/.config/echo-openai-key.sh",
       openai_params = {
         model = "gpt-4-1106-preview",
         frequency_penalty = 0,
