ac063871357571abed287b5f8415854835720749
Ola Flisbäck <ola.flisback@gmail.com>
2024-03-18 09:38:36 +0100

    Disable yaml formatting

--- init.lua
+++ init.lua
@@ -38,6 +38,7 @@ return {
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
           "markdown",
+          "yaml",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
