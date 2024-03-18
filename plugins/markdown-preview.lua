a4a80a8174a9ca17819eb36534fca778df6a4f1c
Ola Flisbäck <ola.flisback@gmail.com>
2024-03-18 09:39:09 +0100

    Fix markdown preview config

--- plugins/markdown-preview.lua
+++ plugins/markdown-preview.lua
@@ -1,12 +1,17 @@
-	-- You may need to manually do yarn install in:
-	-- ~/.local/share/nvim/site/pack/packer/start/markdown-preview.nvim/
-	-- to get it working.
+-- You may need to manually do yarn install in:
+-- ~/.local/share/nvim/site/pack/packer/start/markdown-preview.nvim/
+-- to get it working.
 return {
-		"iamcco/markdown-preview.nvim",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-			vim.g.mkdp_browser = "firefox"
-		end,
-		build = "cd app && npm install",
-		ft = { "markdown" },
-	}
+  "iamcco/markdown-preview.nvim",
+  init = function()
+    vim.cmd [[
+   function OpenMarkdownPreview (url)
+              execute "silent ! open -a Firefox -n --args --new-window " . a:url
+            endfunction
+            let g:mkdp_browserfunc = 'OpenMarkdownPreview'
+    ]]
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  build = "cd app && npm install",
+  ft = { "markdown" },
+}
