ef1b9126b9a8de39b2341b83c6c95b26b6fa9207
Ola Flisbäck <ola.flisback@gmail.com>
2024-03-18 09:38:14 +0100

    Re-enable vim-rooter

--- plugins/vim-rooter.lua
+++ plugins/vim-rooter.lua
@@ -1,7 +1,8 @@
 return {
-	"airblade/vim-rooter",
-	lazy = false,
-	init = function()
-		vim.g.rooter_silent_chdir = 1
-	end,
+  "airblade/vim-rooter",
+  lazy = false,
+  init = function()
+    vim.g.rooter_silent_chdir = 1
+    vim.g.rooter_patterns = { ".git" }
+  end,
 }
