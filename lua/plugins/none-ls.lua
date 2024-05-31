501362405ffe2da986b03b9a7b2bea2a195ddafd
Ola Flisbäck <oflisbaeck@apple.com>
2024-05-31 09:49:14 +0200

    Fix eslint_d

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,6 +3,9 @@
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
     local null_ls = require "null-ls"
@@ -25,6 +28,7 @@ return {
       null_ls.builtins.formatting.black,
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettierd,
+      require "none-ls.diagnostics.eslint_d",
     }
     return config -- return final config table
   end,
